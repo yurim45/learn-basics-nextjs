31e52e0a79c0b9050c24dde1231821139fdab458
yrkim <icho0405@gmail.com>
2021-11-18 15:37:46 +0900

    modify: styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div style={{ padding: '2rem' }}>
       <Head>
         <title>web template</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,32 @@
 import type { NextPage } from 'next';
 import { Flag } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { flexSet } from '../../styles/Variable';
+import { Theme } from '../../styles/theme';
 
 const countries = [{ name: 'South Korea', countryCode: 'kr' }];
 
 const Footer: NextPage = () => {
   return (
-    <footer
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-      }}
-    >
+    <FooterDiv>
       <p>
         Copyrightⓒ 2021. <Flag name='kr' />
         Yr. All rights reserved.
       </p>
-    </footer>
+    </FooterDiv>
   );
 };
 
 export default Footer;
+
+const FooterDiv = styled.footer`
+  ${flexSet('center', 'center')}
+  margin: 20;
+  padding: 20;
+  color: grey;
+  border-top: 1px solid ${({ theme }) => theme.colors.grey};
+
+  p {
+    padding: 1rem 0;
+  }
+`;
