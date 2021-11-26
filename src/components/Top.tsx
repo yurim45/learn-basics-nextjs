7ee8fc4f5142db5ffbe4f1c85df7929c15eb7752
yrkim <icho0405@gmail.com>
2021-11-26 10:55:57 +0900

    modify: add logo link

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,18 +1,23 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { Header } from 'semantic-ui-react';
 import Gnb from './Gnb';
 
 const Head: NextPage = () => {
   return (
     <div style={{ padding: 20 }}>
-      <Header
-        as='h1'
-        textAlign='left'
-        style={{ display: 'flex', alignItems: 'flex-end' }}
-      >
-        <img alt='logo' src='/images/web_logo.png' />
-        Web template
-      </Header>
+      <Link href='/'>
+        <a>
+          <Header
+            as='h1'
+            textAlign='left'
+            style={{ display: 'flex', alignItems: 'flex-end' }}
+          >
+            <img alt='logo' src='/images/web_logo.png' />
+            Web template
+          </Header>
+        </a>
+      </Link>
       <Gnb />
     </div>
   );
