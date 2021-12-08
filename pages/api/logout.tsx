f35dad82fc0bb05d9c4a82eb40ce4c4b5bbb43da
yrkim <icho0405@gmail.com>
2021-12-08 09:19:42 +0900

    불필요 파일 삭제

--- pages/api/logout.tsx
+++ pages/api/logout.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default function (req: any, res: any) {
-if (req.method === 'POST') {
+  if (req.method === 'POST') {
     res.setHeader('Set-Cookie', 'a_name=yrKim;Max-Age=0;HttpOnly,Secure');
     res.statusCode = 200;
     res.json({ message: 'ok' });
-  
+  }
 }
