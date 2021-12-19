0cec333ead4ee93583dc6722836f23df8da5b47c
yrkim <icho0405@gmail.com>
2021-12-19 13:16:47 +0900

    수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable @next/next/no-sync-scripts */
-import type { NextPage } from 'next';
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
-import TabMenu from 'components/TabMenu';
-import Main from './main/index';
-import Aside from 'components/Aside';
-import { getProducts } from '../src/common/api/api';
-import styled from 'styled-components';
-import Footer from 'components/Footer';
+import type { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import TabMenu from "components/TabMenu";
+import Main from "./main/index";
+import Aside from "components/Aside";
+import { getProducts } from "../src/common/api/api";
+import styled from "styled-components";
+import Footer from "components/Footer";
 
 const Home: NextPage = ({ list }: any) => {
   return (
-    <div style={{ padding: '2rem', maxWidth: '1200px', margin: 'auto' }}>
+    <div style={{ padding: "2rem", maxWidth: "1200px", margin: "auto" }}>
       <Head>
         <title>web template</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
