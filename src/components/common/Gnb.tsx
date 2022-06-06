f059005f5fa90b2e697cb6cf1bab6e00fe2cb411
yrkim <icho0405@gmail.com>
2022-06-06 14:22:37 +0900

    fix: error

--- src/components/common/Gnb.tsx
+++ src/components/common/Gnb.tsx
@@ -3,32 +3,32 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 const Gnb = () => {
-  const [activeItem, setActiveItem] = useState<String>('');
+  // const [activeItem, setActiveItem] = useState<String>('');
   const router = useRouter();
 
   return (
     <ul>
       <li
         onClick={() => {
-          setActiveItem('home');
+          // setActiveItem('home');
           router.push('/');
         }}
       />
       <li
         onClick={() => {
-          setActiveItem('product');
+          // setActiveItem('product');
           router.push('/product');
         }}
       />
       <li
         onClick={() => {
-          setActiveItem('mypage');
+          // setActiveItem('mypage');
           router.push('/mypage');
         }}
       />
       <li
         onClick={() => {
-          setActiveItem('admin');
+          // setActiveItem('admin');
           router.push('/admin');
         }}
       />
