9361b784f252d9063aaf71e839fa92b2ae9f0280
yrkim <icho0405@gmail.com>
2022-06-14 17:14:21 +0900

    modify: Slider 수정중

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,48 +1,25 @@
-import type { NextPage } from 'next';
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, ReactNode } from 'react';
+import flex from '@shared/styles/flex';
 import styled from 'styled-components';
 
-type dataType = {
-  id: number;
-  imgUrl: string;
-  imgAlt: string;
+type SliderProps = {
+  children: ReactNode[];
 };
 
-const IMG_DATA: dataType[] = [
-  {
-    id: 0,
-    imgUrl:
-      'https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FtcGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    imgAlt: 'camping',
-  },
-  {
-    id: 1,
-    imgUrl:
-      'https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhbXBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-    imgAlt: 'camping',
-  },
-  {
-    id: 2,
-    imgUrl:
-      'https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fGNhbXBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-    imgAlt: 'camping',
-  },
-];
-
-const Slider = () => {
-  const slideRef: any = useRef(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
+const Slider = ({ children }: SliderProps) => {
+  const slideRef = useRef<HTMLDivElement>(null);
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const handlePrevBtn = () => {
     if (currentSlide === 0) {
-      setCurrentSlide(IMG_DATA.length - 1);
+      setCurrentSlide(children?.length - 1);
     } else {
       setCurrentSlide(currentSlide - 1);
     }
   };
 
   const handleNextBtn = () => {
-    if (currentSlide >= IMG_DATA.length - 1) {
+    if (currentSlide >= children?.length - 1) {
       setCurrentSlide(0);
     } else {
       setCurrentSlide(currentSlide + 1);
@@ -50,65 +27,59 @@ const Slider = () => {
   };
 
   useEffect(() => {
-    slideRef.current.style.transition = `all 0.5s ease-in-out`;
-    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`;
+    // slideRef.current.style.transform = `translateX{${800 * currentSlide}}px`;
   }, [currentSlide]);
 
   return (
-    <SliderWarp>
+    <SliderWarp currentSlide={currentSlide} length={children?.length}>
       {currentSlide > 0 && (
         <button className="prev" onClick={handlePrevBtn}>
           {'<'}
         </button>
       )}
-      {currentSlide < IMG_DATA.length - 1 && (
-        <button
-          className="next"
-          onClick={handleNextBtn}
-          style={{ right: currentSlide > 0 ? '-530px' : '-563px' }}
-        >
+      {currentSlide < children?.length - 1 && (
+        <button className="next" onClick={handleNextBtn}>
           {'>'}
         </button>
       )}
-      <div className="container" ref={slideRef}>
-        {IMG_DATA.map((img) => {
-          return <img key={img.id} alt={img.imgAlt} src={img.imgUrl} />;
-        })}
-      </div>
+      <div className="container">{children}</div>
     </SliderWarp>
   );
 };
 
 export default Slider;
 
-const SliderWarp = styled.div`
-  width: 600px;
-  margin: auto;
+const SliderWarp = styled.div<{ currentSlide: number; length: number }>`
+  width: 1000px;
   overflow: hidden;
 
   .prev,
   .next {
-    position: relative;
+    position: absolute;
     top: 225px;
     padding: 10px 12px 6px;
-    color: ${({ theme }) => theme.colors.gray};
+    color: var(--yellow);
     background-color: rgba(255, 255, 255, 0.5);
     border-radius: 50%;
-    z-index: 100;
+    z-index: 200;
 
     &:hover {
       transition: all 0.3s ease-in-out;
-      background-color: ${({ theme }) => theme.colors.gray};
-      color: #fff;
+      background-color: var(--compo-lightgray);
+      color: var(--white);
     }
   }
   .prev {
     left: 5px;
   }
   .next {
+    right: 805px;
   }
 
   .container {
-    width: 600px;
+    ${flex({ justify: 'start' })};
+    width: ${({ length }) => `${length * 800}px`};
+    transition: all 0.5s ease-in-out;
+    transform: ${({ currentSlide }) => `translateX{${800 * currentSlide}}px`};
   }
 `;
--- src/features/projects/index.tsx
+++ src/features/projects/index.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { Layout } from '@components/index';
 import styled from 'styled-components';
 import { formSet } from '@shared/styles/variables';
+import Slider from '@components/Slider';
 
 const Projects = () => {
   return (
-    <Layout
-      pageTitle="Projects"
-      color="white"
-      lineColor="white"
-      bgColor="black"
-    >
-      <div></div>
-    </Layout>
+    <>
+      <Layout
+        pageTitle="Projects"
+        color="white"
+        lineColor="white"
+        bgColor="black"
+      >
+        <Slider>
+          <StContainer>프로젝ㅌ1</StContainer>
+          <StContainer>프로젝ㅌ2</StContainer>
+          <StContainer>프로젝ㅌ3</StContainer>
+          <StContainer>프로젝ㅌ4</StContainer>
+        </Slider>
+      </Layout>
+    </>
   );
 };
 
@@ -21,4 +29,6 @@ export default Projects;
 const StContainer = styled.div`
   ${formSet()};
   margin: 40px;
+  width: 800px;
+  height: 800px;
 `;
